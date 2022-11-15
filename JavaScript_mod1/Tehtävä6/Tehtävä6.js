113896d0fa325712cece0c39b2f7a436bc090ae6
pete_ <Petri.Kallinen@metropolia.fi>
2022-11-15 07:21:34 +0200

    ekat javat

--- JavaScript_mod1/Tehtävä6/Tehtävä6.js
+++ JavaScript_mod1/Tehtävä6/Tehtävä6.js
@@ -1,17 +1,20 @@
 let query = confirm('Should i calculate the square root?');
 
-console.log(query)
+
 
 print1 = document.querySelector('#target')
 
 
-if (query === true);
-{
-    number = prompt('What number should we calculate?')
+if (query === true){
+    const number = parseInt(prompt('what number do you want to calculate?'))
+    sqr = Math.sqrt(number)
+    print1.innerHTML = 'the square root is ' + sqr
+    if (number < 0){
+    print1.innerHTML = 'the square root of a negative is not defined.'
 }
 
-
-if (query === false);
-{
-    print1.innerHTML = 'the square root is not calculated'
 }
+if (query === false){
+    print1.innerHTML = 'the square root is not calculated.'
+}
+
